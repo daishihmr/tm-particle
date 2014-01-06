f25793698186ae4e728821d0c0cbc40be3bd2093
daishihmr <daishi.hmr@gmail.com>
2014-01-06 19:17:32 +0900

    fix

--- main.js
+++ main.js
@@ -1,40 +1,37 @@
-var app;
-var pe;
+var STATS = false;
 
-// tm.preload(function() {
-//     tm.util.ScriptManager.loadStats();
-// });
+var app;
+var particleElement;
+
+if (STATS) {
+    tm.preload(function() {
+        tm.util.ScriptManager.loadStats();
+    });
+}
 
 tm.main(function() {
     app = tm.display.CanvasApp("#app");
-    // app.enableStats();
+    if (STATS) app.enableStats();
     app.run();
 
-    pe = tm.display.ParticleElement(DATA_DEFAULT)
+    particleElement = tm.display.ParticleElement(DATA_DEFAULT)
         .setPosition(app.width/2, app.height/2)
         .addChildTo(app.currentScene);
 });
 
 var myApp;
 angular.element(document).ready(function() {
-    $("#value-input").tmpl().appendTo("#tmpl-test");
-
-    myApp = angular.module('myApp', []);
+    myApp = angular.module("myApp", []);
     myApp.controller("MainCtrl", ["$scope", function($scope) {
-
-        $scope.limit = 1000;
-        $scope.generate = [];
-        $scope.generateArea = [];
-        $scope.speed = [];
-        $scope.direction = [];
-        $scope.radius = [];
 
         $scope.$extend(DATA_DEFAULT);
 
         $scope.generate[2] = "linear";
 
         $scope.enableEasing = {};
-        $scope.enableEasing.generate = $scope.generate.length > 1;
+        ["generate", "generateArea", "speed", "direction", "radius"].forEach(function(v) {
+            $scope.enableEasing[v] = $scope[v].length > 1;
+        });
 
         $scope.easings = [];
         for (var name in tm.anim.easing) {
@@ -55,8 +52,8 @@ angular.element(document).ready(function() {
         ];
 
         $scope.regenerate = function() {
-            if (pe) pe.remove();
-            pe = tm.display.ParticleElement($scope)
+            if (particleElement) particleElement.remove();
+            particleElement = tm.display.ParticleElement($scope)
                 .setPosition(app.width/2, app.height/2)
                 .addChildTo(app.currentScene);
         };
